355304f970180c38a94118c9d51ae9fc84fa871e
Semih Eryücel <76877013+semihi@users.noreply.github.com>
2023-11-11 22:51:40 +0300

    Updated Progress Bar and Timer Activity Layout

--- SolvingTimer/app/src/main/java/com/example/solvingtimer/MainActivity.kt
+++ SolvingTimer/app/src/main/java/com/example/solvingtimer/MainActivity.kt
@@ -2,11 +2,8 @@ package com.example.solvingtimer
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.NumberPicker
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_launch.*
-import kotlin.concurrent.fixedRateTimer
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,37 +15,11 @@ class MainActivity : AppCompatActivity() {
         numberPicker.maxValue=600
         numberPicker.value=60
 
-
-        Log.d("myLog","0 0 0 0 0")
-
         btnStart.setOnClickListener {
-
             val intent = Intent(this,TimerActivity::class.java)
             intent.putExtra("targetTime",numberPicker.value)
-            Log.d("myLog","1 1 1 1 1")
             finish()
-
-            Log.d("myLog","2 2 2 2 2")
             startActivity(intent)
-            Log.d("myLog","3 3 3 3 3")
-
         }
-
-
-//            progressBar.max = Integer.parseInt(inputMax.getText().toString());
-
-//        btnStart.setOnClickListener() {
-//
-//            if (isRunning == false) {
-//                isRunning = true
-//
-
-
-
-
-
-
-//            }
-//        }
     }
 }
--- SolvingTimer/app/src/main/java/com/example/solvingtimer/TimerActivity.kt
+++ SolvingTimer/app/src/main/java/com/example/solvingtimer/TimerActivity.kt
@@ -2,11 +2,15 @@ package com.example.solvingtimer
 
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_timer.btnNext
 import kotlinx.android.synthetic.main.activity_timer.btnStartPause
 import kotlinx.android.synthetic.main.activity_timer.progressBarFirst
+import kotlinx.android.synthetic.main.activity_timer.progressBarSecond
+import kotlinx.android.synthetic.main.activity_timer.progressBarThird
+import kotlinx.android.synthetic.main.activity_timer.textNumQuestion
 import kotlinx.android.synthetic.main.activity_timer.textTimer
+import kotlinx.android.synthetic.main.activity_timer.textTotalTime
 
 class TimerActivity : AppCompatActivity() {
 
@@ -14,37 +18,67 @@ class TimerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
 
-        Log.d("myLog","4 4 4 4 4")
-
         var intent = intent
         var targetTime = intent.getIntExtra("targetTime",60)
         progressBarFirst.max=targetTime
+        progressBarSecond.max=targetTime
+        progressBarThird.max=targetTime
 
-        Log.d("myLog",targetTime.toString())
-
-        var duration: Int = 0
-        var prog: Int = 0
-        var running: Boolean = true
+        var running: Boolean = false
         var seconds: Int = 0
+        var Startion: Int = 0
+        val handler = Handler()
+        var minutes = 0
+        var secs = 0
+        var numQuestion = 1
 
-        btnStartPause.setOnClickListener {
-
-            val handler = Handler()
+        fun startTimer() {
+            btnStartPause.text = "Duraklat"
+            running = true
+            seconds--
             handler.post(object : Runnable {
                 override fun run() {
                     if (running) {
                         seconds++
-                        val minutes = seconds / 60
-                        val secs = seconds % 60
+                        minutes = (seconds-Startion) / 60
+                        secs = (seconds-Startion) % 60
 
                         textTimer.text = String.format("%2d:%02d", minutes, secs)
+                        progressBarFirst.progress = seconds-Startion
+                        progressBarSecond.progress = seconds-Startion-targetTime
+                        progressBarThird.progress = seconds-Startion-2*targetTime
+
+                        textTotalTime.text = String.format("%02d:%02d", seconds/60, seconds % 60)
 
                         handler.postDelayed(this, 1000)
                     }
                 }
             })
+        }
 
+        fun pauseTimer() {
+            btnStartPause.text = "Devam Et"
+            running = false
+            handler.removeCallbacksAndMessages(null)
+        }
 
+        btnNext.setOnClickListener {
+            pauseTimer()
+            startTimer()
+
+            numQuestion++
+            textNumQuestion.text = numQuestion.toString()+". Soru"
+            Startion = seconds+1
+            minutes = 0
+            secs = 0
+        }
+
+        btnStartPause.setOnClickListener {
+            if (!running) {
+                startTimer()
+            } else {
+                pauseTimer()
+            }
         }
     }
 }
